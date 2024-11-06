b776d2d649b7e4bdbbb90e13c1567c50c1209b15
Erick Araya <95132759+Francolin09@users.noreply.github.com>
2024-11-06 17:28:02 -0300

    Conexion inicial back-front de company

--- gipit-react/app/actions/fetchCompanies.tsx
+++ gipit-react/app/actions/fetchCompanies.tsx
@@ -1,4 +1,5 @@
 "use server";
+import { InnerTabProps } from "../lib/types";
 import { fetchFirstC, fetchAllC, fetchCDetails } from "./fakeApi";
 
 export const fetchFirstCompany = async () => {
@@ -6,12 +7,40 @@ export const fetchFirstCompany = async () => {
   return res;
 };
 
-export const fetchAllCompanies = async () => {
-  const res = await fetchAllC();
-  return res;
+// export const fetchAllCompanies = async () => {
+//   const res = await fetchAllC();
+//   return res;
+// };
+
+
+export const fetchAllCompanies = async (): Promise<InnerTabProps[]> => {
+  try {
+    const response = await fetch(` http://localhost:3001/api/company`, {
+       headers:{
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEwLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MzA5MjM4ODMsImV4cCI6MTczMDkyNzQ4M30.zstGs5R-CjI62d7qojWSwWQhoSFVgEICkyL1CoPwKu0',
+        'Content-Type': 'application/json'
+       }
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch companies");
+    }
+
+    const companies = await response.json();
+    console.log(companies)
+    return companies.map((company: any) => ({
+      id: company.id,
+      name: company.name,
+    }));
+
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 export const fetchCompanyDetails = async (id: number) => {
   const res = await fetchCDetails(id);
   return res;
 };
+
