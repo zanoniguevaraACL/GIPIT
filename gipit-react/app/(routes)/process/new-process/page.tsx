6cc73180ac7ba5f80d2544e291deff7bbc1a0549
Bastian-Bouffanais <bastibouffanais@gmail.com>
2024-11-28 15:59:27 -0300

    ahora si

--- gipit-react/app/(routes)/process/new-process/page.tsx
+++ gipit-react/app/(routes)/process/new-process/page.tsx
@@ -1,11 +1,47 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import Modal from "@/components/molecules/Modal";
 import { FormInputsRow } from "@/app/lib/types";
 import { handleCreateProcess } from "@/app/actions/handleCreateProcess";
 import { fetchListCompanies } from "@/app/actions/fetchCompanies";
 
-async function Page() {
-  const clientsList = await fetchListCompanies();
-  console.log(clientsList);
+type Client = {
+  id: number;
+  name: string;
+};
+
+const Page = () => {
+  const [clientsList, setClientsList] = useState<Client[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getClients = async () => {
+      try {
+        const data = await fetchListCompanies();
+        setClientsList(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError("Error al obtener la lista de compañías: " + error.message);
+        } else {
+          setError("Error al obtener la lista de compañías.");
+        }
+      } finally {
+        setIsLoading(false); 
+      }
+    };
+
+    getClients(); 
+  }, []); 
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   const selectFieldOptions = clientsList.map((client) => ({
     name: client.name,
     value: client.id,
@@ -41,6 +77,6 @@ async function Page() {
   return (
     <Modal rows={fields} onSubmit={handleCreateProcess} title="Nuevo Proceso" />
   );
-}
+};
 
 export default Page;
