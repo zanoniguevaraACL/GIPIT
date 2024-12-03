2a6895425b8a3d194aa8d5b87218f9493948af3f
Bastian-Bouffanais <bastibouffanais@gmail.com>
2024-12-03 15:23:42 -0300

    pantalla principal facturas funcionando

---
+++ gipit-react/app/actions/fetchInvoices.tsx
@@ -0,0 +1,32 @@
+export const fetchAllPreInvoices = async (page: number) => {
+    try {
+      const apiUrl = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/preinvoices?page=${page}`;
+    
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        cache: 'no-store',
+      });
+    
+      if (!response.ok) {
+        throw new Error('Error al obtener las facturas');
+      }
+    
+      const preInvoicesList = await response.json();
+    
+      if (!Array.isArray(preInvoicesList.batch)) {
+        throw new Error('se esperaba un array en el batch');
+      }
+    
+      return preInvoicesList.batch; 
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(`Error al obtener las facturas: ${error.message || 'Error desconocido'}`);
+      } else {
+        throw new Error('Error desconocido al obtener las prefacturas');
+      }
+    }
+  };
+  
