1fc12dfdd0c09a17252c7b65ddae80f1e8e591f6
ACLSebastianPinto <101645781+Sekod@users.noreply.github.com>
2024-12-23 11:39:17 -0300

    actualizado front

--- gipit-react/app/(routes)/invoices/[invoiceId]/page.tsx
+++ gipit-react/app/(routes)/invoices/[invoiceId]/page.tsx
@@ -1,4 +1,4 @@
-import { fetchInvoiceDetails } from "@/app/actions/fakeApi";
+import { fetchAllPreInvoices } from "@/app/actions/fetchInvoices";
 import "../../pros/[proId]/proId.css";
 import "./invoice.css";
 import "@/components/molecules/candidateClientNote.css";
@@ -8,20 +8,32 @@ import { IconReceipt } from "@tabler/icons-react";
 
 interface InvoiceDetails {
   id: number;
-  name: string;
-  service: string;
-  rate: number;
-  hours: number;
-  subtotal: number;
-  vat: number;
-  total: number;
+  estimated_date: string;
+  expiration_date: string;
+  total_value: string;
   description: string;
+  status: string;
+  candidateName: string;
+  date: string;
+  rate: string;
+  subtotal: string;
+  pre_invoice_items: Array<{
+    id: number;
+    service: string;
+    rate: string;
+    hours: string;
+    subtotal: string;
+    vat: string;
+    total: string;
+    description: string;
+    candidates: Array<{ name: string }>;
+  }>;
 }
 
 interface Column<T> {
   name: string;
-  key: keyof T; // Clave que referencia la propiedad del objeto T
-  width: number; // Ancho de la columna
+  key: keyof T;
+  width: number;
 }
 
 interface ResponseData<T> {
@@ -36,39 +48,60 @@ export default async function Page(props: {
   };
 }) {
   const { invoiceId } = props.params;
+  const invoicesList = await fetchAllPreInvoices(1);
+  const invoiceDetails = invoicesList.find((invoice: InvoiceDetails) => invoice.id === parseInt(invoiceId));
 
-  const invoiceDetails = await fetchInvoiceDetails(parseInt(invoiceId));
-
+  if (!invoiceDetails) {
+    return <div>No se encontró la factura.</div>;
+  }
+//SE CREA EL DATA GRID
   const dataGridData: ResponseData<InvoiceDetails> = {
     columns: [
-      { name: "Nombre", key: "name", width: 1.3 },
-      { name: "Rol", key: "service", width: 1.5 },
-      { name: "Valor HH", key: "rate", width: 1 },
-      { name: "Horas trabajadas", key: "hours", width: 1 },
-      { name: "Vat", key: "vat", width: 1 },
-      { name: "Total", key: "total", width: 1 },
+      { name: "Nombre", key: "candidateName", width: 1.5 },
+      { name: "Fecha", key: "date", width: 1 },
+      { name: "UF por hora", key: "rate", width: 1 },
+      { name: "Subtotal", key: "subtotal", width: 1 },
     ],
-    total: 12,
-    batch: invoiceDetails.details,
+    total: invoiceDetails.pre_invoice_items ? invoiceDetails.pre_invoice_items.length : 0,
+    batch: invoiceDetails.pre_invoice_items ? invoiceDetails.pre_invoice_items.map((item) => ({
+      id: item.id,
+      candidateName: item.candidates.length > 0 ? item.candidates[0].name : "Sin candidato",
+      date: new Date(invoiceDetails.estimated_date).toLocaleDateString(),
+      rate: parseFloat(item.rate),
+      subtotal: parseFloat(item.subtotal),
+    })) : [],
   };
+
+
+  //SE LLAMA A LA API PARA OBTENER EL TOTAL DE LA FACTURA
+  const totalInvoice = invoiceDetails.pre_invoice_items ? invoiceDetails.pre_invoice_items.reduce((acc: number, item: { total: string }) => acc + parseFloat(item.total), 0) : 0;
+
+
+  //SE OBTIENE EL NOMBRE DE LOS CANDIDATOS
+  const candidates = invoiceDetails.pre_invoice_items.flatMap(item => item.candidates || []);
+  const professionalsDisplay = candidates.length > 0 
+    ? candidates.map(candidate => candidate.name).join(', ') 
+    : 'Sin candidatos';
 
   return (
     <div className="max-container">
       <div className="pro-details-container">
-        {/* Header */}
         <div className="pro-header">
           <div className="pro-avatar">
             <IconReceipt />
           </div>
           <div>
-            <div className="flex-row gap-12 name-n-mail-container">
-              <h4>Pre Factura</h4>
-            </div>
+            <h4>Pre Factura</h4>
             <p className="text-12 position">
-              Emitido el 8/02/2025 - Válido hasta el 12/02/2025
+              Emitido el {new Date(invoiceDetails.estimated_date).toLocaleDateString()} - Válido hasta {new Date(invoiceDetails.expiration_date).toLocaleDateString()}
             </p>
           </div>
           <div className="pro-buttons-container">
+            <Button
+              text="Eliminar Factura"
+              href={`/invoices/${invoiceId}/delete`}
+              type="primary"
+            />
             <Button
               text="Notificar un error"
               href={`/invoices/${invoiceId}/notify`}
@@ -76,29 +109,24 @@ export default async function Page(props: {
             />
           </div>
         </div>
-        <br></br>
-        {/* Detalle de la factura */}
         <p className="section-title text-14">Detalles de la factura</p>
+        <p>Profesionales: {professionalsDisplay}</p>
         <DataGrid
           data={dataGridData}
           baseUrl={`/pros/${invoiceId}`}
           hasNoClick={true}
           hasNoPagination={true}
         />
-        {/* Total */}
         <div className="total-container">
           <h3>Total a pagar</h3>
-          <h1>{invoiceDetails.total}</h1>
+          <h1>{totalInvoice.toFixed(2)} UF</h1>
         </div>
-        {/* boton de comprobar */}
         <div className="client-note-container">
           <div className="note-header">
             <h4>Nota</h4>
           </div>
           <p className="text-14">
-            Este resumen es una instancia para validar el monto a facturar y los
-            detalles de la facturación, al confirmarlos procederemos a enviar
-            los documentos legales correspondientes.
+            Este resumen es una instancia para validar el monto a facturar y los detalles de la facturación, al confirmarlos procederemos a enviar los documentos legales correspondientes.
           </p>
           <div className="button-note-container">
             <Button
--- gipit-react/app/(routes)/invoices/page.tsx
+++ gipit-react/app/(routes)/invoices/page.tsx
@@ -4,11 +4,13 @@ import SearchBar from "@/components/molecules/SearchBar";
 
 interface InvoiceDetails {
   id: number;
-  cantidad: number;
+  professionals: string;
+  period: string;
   total_value: number;
+  date: string;
+  status: string;
   estimated_date: string;
   expiration_date: string;
-  status: string;
 }
 
 interface Column<T> {
@@ -31,31 +33,48 @@ export default async function Page(props: {
 }) {
   const searchParams = await props.searchParams;
   const page = searchParams?.page ? parseInt(searchParams?.page) : 1;
+  const query = searchParams?.query || "";
 
   const invoicesList = await fetchAllPreInvoices(page);
-  
-
-  const total = 24; 
-  
   const data: ResponseData<InvoiceDetails> = {
     columns: [
-      { name: "Fecha", key: "estimated_date", width: 1.1 },
-      { name: "Exipra", key: "expiration_date", width: 1.1 },
-      { name: "Profesionales", key: "cantidad", width: 1 },
-      { name: "Monto", key: "total_value", width: 1 },
+      { name: "Profesionales", key: "professionals", width: 2 },
+      { name: "Período", key: "period", width: 1.5 },
+      { name: "Monto (UF)", key: "total_value", width: 1 },
+      { name: "Fecha", key: "expiration_date", width: 1 },
       { name: "Estado", key: "status", width: 1 },
     ],
-    total, 
-    batch: invoicesList, 
+    total: invoicesList.length,
+    batch: invoicesList.map((invoice: InvoiceDetails) => {
+      const estimatedDate = new Date(invoice.estimated_date);
+      const expirationDate = new Date(invoice.expiration_date);
+      
+      const period = `${estimatedDate.toLocaleString('default', { month: 'long' })} - ${expirationDate.toLocaleString('default', { month: 'long' })}`;
+
+      const candidates = invoice.professionals.split(',').map(professional => professional.trim());
+      const professionals = candidates.slice(0, 3);
+      const additionalCount = candidates.length - 3;
+
+      const professionalsDisplay = additionalCount > 0 
+        ? `${professionals.join(', ')} y otros ${additionalCount}` 
+        : professionals.join(', ');
+
+      return {
+        ...invoice,
+        period, // Agregar el nuevo campo period
+        expiration_date: expirationDate.toISOString().split('T')[0], // Extraer solo el año, mes y día
+        professionals: professionalsDisplay,
+      };
+    }),
   };
 
   return (
     <div className="inner-page-container">
-      <SearchBar
-        buttonLink="/invoices/new-invoice"
-        buttonText="Nueva Factura"
+      <SearchBar buttonLink="/invoices/new-invoice" buttonText="Nueva Factura" />
+      <DataGrid 
+        data={data} 
+        baseUrl="/invoices"
       />
-      <DataGrid data={data} baseUrl="/invoices" />
     </div>
   );
 }
