6f1414d79619e90640337ddaf34f0938288b3fc9
ACLSebastianPinto <101645781+Sekod@users.noreply.github.com>
2025-01-20 16:01:00 -0300

    creacion de usuario
    creacion de usuario funcional

--- gipit-react/app/actions/fetchUsers.tsx
+++ gipit-react/app/actions/fetchUsers.tsx
@@ -8,24 +8,32 @@ interface User {
     };
 }
 
-export const fetchUsers = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`);
+export const fetchUsers = async (page: number, query?: string, role?: string) => {
+    const params = new URLSearchParams();
+    params.append('page', page.toString());
+    if (query) params.append('query', query);
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users?${params.toString()}`);
     if (!response.ok) {
         throw new Error("Error al obtener usuarios");
     }
     
     const data = await response.json();
     
-    // Verifica que la respuesta contenga un array de usuarios
-    if (!Array.isArray(data.users)) {
-        throw new Error("La respuesta no contiene un array de usuarios");
+    return {
+        total: data.total,
+        batch: data.users.map((user: User) => ({
+            ...user,
+            roleName: user.roles?.nombre || "Sin rol"
+        })),
+    };
+};
+
+export const fetchRoles = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/roles`);
+    if (!response.ok) {
+        throw new Error('Error al obtener roles');
     }
-
-    // Mapea los usuarios para incluir el nombre del rol
-    const usersWithRoles = data.users.map((user: User) => ({
-        ...user,
-        roleName: user.roles?.nombre || "Sin rol"
-    }));
-
-    return usersWithRoles;
+    const data = await response.json();
+    return data;
 };
