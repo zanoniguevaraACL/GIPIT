147727f9f0d63d6a726aa7c1dc49e48e4397da6c
ACLVictorNavarro <vnavarro@acl.tech>
2025-01-23 11:32:33 -0300

    Documentación

--- gipit-react/app/(routes)/docs/page.tsx
+++ gipit-react/app/(routes)/docs/page.tsx
@@ -4,7 +4,11 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 export default function ApiDocs() {
+  const baseUrl = process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:3001'
+    : 'https://gipit-back.vercel.app';
+
   return (
-    <SwaggerUI url={`${process.env.NEXT_PUBLIC_API_URL}/api/docs`} />
+    <SwaggerUI url={`${baseUrl}/api/docs`} />
   );
 }
